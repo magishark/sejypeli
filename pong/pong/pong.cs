bd83ca0c075c9cc9f633ade343589783d51dd46e
magishark <magishark001@gmail.com>
2013-09-19 08:18:06 -0700

    MElkein Beli Walbis

--- pong/pong/pong.cs
+++ pong/pong/pong.cs
@@ -8,34 +8,53 @@ using Jypeli.Widgets;
 
 public class pong : PhysicsGame
 {
+    Vector nopeusYlos = new Vector(0, 200);
+    Vector nopeusAlas = new Vector(0, -200);
+
     PhysicsObject Ball;
+    PhysicsObject maila1;
+    PhysicsObject maila2;
+
+    IntMeter pelaajan1pisteet;
+    IntMeter pelaajan2Pisteet;
 
     public override void Begin()
     {
         LuoKentta();
         AsetaOhjaimet();
+        LisaaLaskurit();
         AloitaPeli();
+    }
 
-        void AsetaOhjaimet()
-        {
-            Keyboard.Listen(Key.A, ButtonState.Down; LiikutaMaila1Ylös, "pelaaja 1: Liikuta mailaa ylös");
-            Keyboard.Listen(Key.A; ButtonState.Released; PysautaMaila1, null);
+    void AsetaOhjaimet()
+    {
+        Keyboard.Listen(Key.A, ButtonState.Down, AsetaNopeus, "pelaaja 1: Liikuta mailaa ylös", maila1, nopeusYlos);
+        Keyboard.Listen(Key.A, ButtonState.Released, AsetaNopeus, null, maila1, Vector.Zero);
+        Keyboard.Listen(Key.Z, ButtonState.Down, AsetaNopeus, "Pelaaja 1: Liikuta mailaa alas", maila1, nopeusAlas);
+        Keyboard.Listen(Key.Z, ButtonState.Released, AsetaNopeus, null, maila1, Vector.Zero);
 
+        Keyboard.Listen(Key.Up, ButtonState.Down, AsetaNopeus, "Pelaaja 2: Liikuta mailaa ylös", maila2, nopeusYlos);
+        Keyboard.Listen(Key.Up, ButtonState.Released, AsetaNopeus, null, maila2, Vector.Zero);
+        Keyboard.Listen(Key.Down, ButtonState.Down, AsetaNopeus, "Pelaaja 2: Liikuta mailaa alas", maila2, nopeusAlas);
+        Keyboard.Listen(Key.Down, ButtonState.Released, AsetaNopeus, null, maila2, Vector.Zero);
+
+        Keyboard.Listen(Key.F1, ButtonState.Pressed, ShowControlHelp, "Näytä ohjeet");
         Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
     }
+
     void LuoKentta()
     {
-         Ball = new PhysicsObject(50, 50);
+        Ball = new PhysicsObject(50, 50);
         Ball.Restitution = 1.0;
         Ball.Shape = Shape.Circle;
         Ball.X = -200.0;
         Ball.Y = 0.0;
         Add(Ball);
-        
-        LuoMaila(Level.Left + 20.0, 0.0);
-        LuoMaila(Level.Right - 20.0, 0.0);
 
-       
+        maila1 = LuoMaila(Level.Left + 20.0, 0.0);
+        maila2 = LuoMaila(Level.Right - 20.0, 0.0);
+
+
 
         Level.CreateBorders(1.0, false);
         Level.BackgroundColor = Color.YellowGreen;
@@ -48,7 +67,7 @@ public class pong : PhysicsGame
         Vector impulssi = new Vector(500.0, 0.0);
         Ball.Hit(impulssi);
     }
-    void LuoMaila(double x, double y)
+    PhysicsObject LuoMaila(double x, double y)
     {
         PhysicsObject maila = PhysicsObject.CreateStaticObject(20.0, 100.0);
         maila.Shape = Shape.Rectangle;
@@ -56,5 +75,42 @@ public class pong : PhysicsGame
         maila.Y = y;
         maila.Restitution = 1.0;
         Add(maila);
+        return maila;
+    }
+    void AsetaNopeus(PhysicsObject maila, Vector nopeus)
+    {
+        if ((nopeus.Y < 0) && (maila.Bottom < Level.Bottom))
+        {
+            maila.Velocity = Vector.Zero;
+            return;
+        }
+        if ((nopeus.Y > 0) && (maila.Top > Level.Top))
+        {
+            maila.Velocity = Vector.Zero;
+            return;
+        }
+
+        maila.Velocity = nopeus;
+    }
+    void LisaaLaskurit()
+    {
+        pelaajan1pisteet = LuoPisteLaskuri(Screen.Left + 100.0, Screen.Top - 100.0);
+        pelaajan2Pisteet = LuoPisteLaskuri(Screen.Right - 100.0, Screen.Top - 100.0);
+    }
+    IntMeter LuoPisteLaskuri(double x, double y)
+    {
+        IntMeter laskuri = new IntMeter(0);
+        laskuri.MaxValue = 10;
+
+        Label naytto = new Label();
+        naytto.BindTo(laskuri);
+        naytto.X = x;
+        naytto.Y = y;
+        naytto.TextColor = Color.Black;
+        naytto.BorderColor = Level.BackgroundColor;
+        naytto.Color = Level.BackgroundColor;
+        Add(naytto);
+
+        return laskuri;
     }
 }
