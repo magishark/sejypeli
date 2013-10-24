8be11d502a9e11c36d02e8eabe68192e5d939f52
magishark <magishark001@gmail.com>
2013-10-24 08:25:16 -0700

    Tein Peliä XD

---
+++ Brains on the Field/Brains on the Field/Brains_on_the_Field.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Jypeli;
+using Jypeli.Assets;
+using Jypeli.Controls;
+using Jypeli.Effects;
+using Jypeli.Widgets;
+
+public class Brains_on_the_Field : PhysicsGame
+{
+    public override void Begin()
+    {
+        Surface alaReuna = Surface.CreateBottom(Level);
+        Add(alaReuna);
+
+        alaReuna.Color = Color.Black;
+
+       Surface vasenReuna = Surface.CreateLeft(Level);
+       Add(vasenReuna);
+
+        vasenReuna.Color = Color.Black;
+
+       Surface oikeaReuna = Surface.CreateRight(Level);
+       Add(oikeaReuna);
+
+        oikeaReuna.Color = Color.Black;
+
+       Surface ylaReuna = Surface.CreateTop(Level);
+       Add(ylaReuna);
+
+       ylaReuna.Color = Color.Black;
+
+        // TODO: Kirjoita ohjelmakoodisi tähän
+
+        PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
+        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+    }
+}
---
+++ Brains on the Field/Brains on the Field/Ohjelma.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+static class Ohjelma
+{
+#if WINDOWS || XBOX
+    static void Main(string[] args)
+    {
+        using (Brains_on_the_Field game = new Brains_on_the_Field())
+        {
+#if !DEBUG
+            game.IsFullScreen = true;
+#endif
+            game.Run();
+        }
+    }
+#endif
+}
